38a2f6f567701a8512ec880113a02be09dd01e35
ibmbt <ibrahimtahirbutt10@gmail.com>
2025-12-08 13:54:01 +0500

    serialize deserialize

--- graph/node.h
+++ graph/node.h
@@ -7,6 +7,30 @@
 #include "edge.h"
 using namespace std;
 
+inline void writeString(char*& buffer, const string& s) {
+    uint32_t len = static_cast<uint32_t>(s.length());
+    memcpy(buffer, &len, sizeof(uint32_t));
+    buffer += sizeof(uint32_t);
+
+    if (len > 0) {
+        memcpy(buffer, s.c_str(), len);
+        buffer += len;
+    }
+}
+
+inline string readString(const char*& buffer) {
+    uint32_t len;
+    memcpy(&len, buffer, sizeof(uint32_t));
+    buffer += sizeof(uint32_t);
+
+    string s = "";
+    if (len > 0) {
+        s.resize(len);
+        memcpy(&s[0], buffer, len);
+        buffer += len;
+    }
+    return s;
+}
 class Movie {
 public:
     uint32_t movieID;
@@ -58,8 +82,70 @@ public:
     }
 
 
-    void serialize(char* buffer, size_t& size) const;
-    static Movie deserialize(const char* buffer, size_t& bytesRead);
+    void serialize(char* buffer, size_t& size) const {
+        char* originalStart = buffer;
+
+        memcpy(buffer, &movieID, sizeof(uint32_t));
+        buffer = buffer + sizeof(uint32_t);
+
+        writeString(buffer, title);
+
+        uint32_t genreCount = static_cast<uint32_t>(genres.size());
+        memcpy(buffer, &genreCount, sizeof(uint32_t));
+        buffer = buffer + sizeof(uint32_t);
+
+        for (size_t i = 0; i < genres.size(); i++) {
+            writeString(buffer, genres[i]);
+        }
+
+        memcpy(buffer, &avgRating, sizeof(float));
+        buffer = buffer + sizeof(float);
+
+        memcpy(buffer, &ratingCount, sizeof(uint32_t));
+        buffer = buffer + sizeof(uint32_t);
+
+        memcpy(buffer, &ratedByUsersOffset, sizeof(uint64_t));
+        buffer = buffer + sizeof(uint64_t);
+
+        memcpy(buffer, &similarMoviesOffset, sizeof(uint64_t));
+        buffer = buffer + sizeof(uint64_t);
+
+        size = static_cast<size_t>(buffer - originalStart);
+    }
+
+    static Movie deserialize(const char* buffer, size_t& bytesRead) {
+        const char* originalStart = buffer;
+        Movie m;
+
+        memcpy(&m.movieID, buffer, sizeof(uint32_t));
+        buffer = buffer + sizeof(uint32_t);
+
+        m.title = readString(buffer);
+
+        uint32_t genreCount;
+        memcpy(&genreCount, buffer, sizeof(uint32_t));
+        buffer = buffer + sizeof(uint32_t);
+
+        for (size_t i = 0; i < genreCount; i++) {
+            string g = readString(buffer);
+            m.genres.push_back(g);
+        }
+
+        memcpy(&m.avgRating, buffer, sizeof(float));
+        buffer = buffer + sizeof(float);
+
+        memcpy(&m.ratingCount, buffer, sizeof(uint32_t));
+        buffer = buffer + sizeof(uint32_t);
+
+        memcpy(&m.ratedByUsersOffset, buffer, sizeof(uint64_t));
+        buffer = buffer + sizeof(uint64_t);
+
+        memcpy(&m.similarMoviesOffset, buffer, sizeof(uint64_t));
+        buffer = buffer + sizeof(uint64_t);
+
+        bytesRead = static_cast<size_t>(buffer - originalStart);
+        return m;
+    }
 };
 
 class User {
@@ -123,8 +209,55 @@ public:
     }
 
 
-    void serialize(char* buffer, size_t& size) const;
-    static User deserialize(const char* buffer, size_t& bytesRead);
+    void serialize(char* buffer, size_t& size) const {
+        char* originalStart = buffer;
+
+        memcpy(buffer, &userID, sizeof(uint32_t));
+        buffer = buffer + sizeof(uint32_t);
+
+        writeString(buffer, username);
+
+        uint32_t ratingsCount = static_cast<uint32_t>(ratings.size());
+        memcpy(buffer, &ratingsCount, sizeof(uint32_t));
+        buffer = buffer + sizeof(uint32_t);
+
+        for (size_t i = 0; i < ratings.size(); i++) {
+            memcpy(buffer, &ratings[i], sizeof(RatingEntry));
+            buffer = buffer + sizeof(RatingEntry);
+        }
+
+        memcpy(buffer, &ratedMoviesOffset, sizeof(uint64_t));
+        buffer = buffer + sizeof(uint64_t);
+
+        size = static_cast<size_t>(buffer - originalStart);
+    }
+
+    static User deserialize(const char* buffer, size_t& bytesRead) {
+        const char* originalStart = buffer;
+        User u;
+
+        memcpy(&u.userID, buffer, sizeof(uint32_t));
+        buffer = buffer + sizeof(uint32_t);
+
+        u.username = readString(buffer);
+
+        uint32_t ratingsCount;
+        memcpy(&ratingsCount, buffer, sizeof(uint32_t));
+        buffer = buffer + sizeof(uint32_t);
+
+        u.ratings.resize(ratingsCount);
+
+        for (size_t i = 0; i < ratingsCount; i++) {
+            memcpy(&u.ratings[i], buffer, sizeof(RatingEntry));
+            buffer = buffer + sizeof(RatingEntry);
+        }
+
+        memcpy(&u.ratedMoviesOffset, buffer, sizeof(uint64_t));
+        buffer = buffer + sizeof(uint64_t);
+
+        bytesRead = static_cast<size_t>(buffer - originalStart);
+        return u;
+    }
 };
 
 #endif
